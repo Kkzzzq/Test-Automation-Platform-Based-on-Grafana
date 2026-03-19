022bc087c457e56e4766bf1cbbd1169815721cef
Kkzzzq <snorris068@gmail.com>
2026-03-19 21:51:17 +0800

    Relax dashboard auth error schema

--- helpers/schemas/dashboards_schema.py
+++ helpers/schemas/dashboards_schema.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
-from typing import List
+
 
 class GetDashboardSchema(BaseModel):
     id: int
     timezone: str
-    title: str = 'Dashboard for API'
+    title: str = "Dashboard for API"
     uid: str
 
+
 class GetDashboardsWithIncorrectCredentialsSchema(BaseModel):
-    message: str = "Invalid username or password"
-    messageId: str = "password-auth.failed"
+    message: str = "invalid username or password"
+    messageId: str | None = None
