1e8b2929ee26c82c6de978da36a2fa7d308424c1
Kkzzzq <snorris068@gmail.com>
2026-03-24 14:22:27 +0800

    Update test_api_organizations_positive.py

--- tests/test_api_organizations_positive.py
+++ tests/test_api_organizations_positive.py
@@ -1,66 +1,69 @@
 import allure
 import pytest
 
-from data.users_credentials import organizations_user
 from helpers.schemas.organizations_schema import (
     AddUserInOrganizations,
+    CreateOrganizationResponse,
     GetOrganizationsById,
     UpdateUserInOrg,
 )
 from services.api_organizations_service import ApiOrganizationsService
-from services.utils import assert_json_response, validate_status_code_and_body
-
-pytestmark = pytest.mark.usefixtures("session_resources")
 
 
-@allure.title("Test add new user in organization")
-@allure.description("This test attempt to add new user in organization")
-@allure.tag("ApiOrganizationsService", "Positive")
-@allure.id("add_user_in_organization")
-@pytest.mark.PositiveApi
-def test_add_user_in_organization(test_context):
-    response, user_id = ApiOrganizationsService.add_user_in_organization(
-        org_id=test_context.organizations.org_id,
-        body={"loginOrEmail": organizations_user["login"], "role": "Editor"},
+@allure.title("Create organization successfully")
+@pytest.mark.PositiveOrganizations
+@pytest.mark.smoke
+def test_create_organization_success(session_context):
+    response = ApiOrganizationsService.get_organization_by_id(session_context.org_id)
+    assert response.status_code == 200
+
+    data = GetOrganizationsById(**response.json())
+    assert data.id == session_context.org_id
+    assert data.name == session_context.organizations.org_name
+
+
+@allure.title("Add user to organization successfully")
+@pytest.mark.PositiveOrganizations
+def test_add_user_in_organization(session_context):
+    response = ApiOrganizationsService.add_user_in_organization(
+        org_id=session_context.org_id,
+        user_id=session_context.organizations_user_id,
+        role="Viewer",
     )
-    validate_status_code_and_body(response, AddUserInOrganizations, 200)
-    assert_json_response(response)
-    assert user_id == test_context.users.organizations_user_id
+    assert response.status_code in (200, 409)
+
+    if response.status_code == 200:
+        data = AddUserInOrganizations(**response.json())
+        assert data.userId == session_context.organizations_user_id
 
 
-@allure.title("Test get organizations by id")
-@allure.description("This test attempt get organizations by id")
-@allure.tag("ApiOrganizationsService", "Positive")
-@allure.id("get_organizations_by_id")
-@pytest.mark.PositiveApi
-def test_get_organizations_by_id(test_context):
-    response = ApiOrganizationsService.get_organizations_by_id(test_context.organizations.org_id)
-    validate_status_code_and_body(response, GetOrganizationsById, 200)
-    assert_json_response(response)
-    assert response.json()["name"] == test_context.organizations.org_name
-
-
-@allure.title("Test update user permissions in org")
-@allure.description("This test attempt update user permissions in org")
-@allure.tag("ApiOrganizationsService", "Positive")
-@allure.id("update_user_in_org")
-@pytest.mark.PositiveApi
-def test_update_user_in_org(test_context):
-    response = ApiOrganizationsService.update_user_in_org(
-        org_id=test_context.organizations.org_id,
-        user_id=test_context.users.organizations_user_id,
-        role="Admin",
+@allure.title("Update user role in organization successfully")
+@pytest.mark.PositiveOrganizations
+def test_update_user_in_org(session_context):
+    add_response = ApiOrganizationsService.add_user_in_organization(
+        org_id=session_context.org_id,
+        user_id=session_context.organizations_user_id,
+        role="Viewer",
     )
-    validate_status_code_and_body(response, UpdateUserInOrg, 200)
-    assert_json_response(response)
+    assert add_response.status_code in (200, 409)
 
-    users_response = ApiOrganizationsService.get_users_in_organization(
-        test_context.organizations.org_id
+    response = ApiOrganizationsService.update_user_role_in_organization(
+        org_id=session_context.org_id,
+        user_id=session_context.organizations_user_id,
+        role="Editor",
     )
-    assert users_response.status_code == 200
-    target_user = next(
-        user
-        for user in users_response.json()
-        if user["userId"] == test_context.users.organizations_user_id
-    )
-    assert target_user["role"] == "Admin"
+    assert response.status_code == 200
+
+    data = UpdateUserInOrg(**response.json())
+    assert data.message
+
+
+@allure.title("Get organization by id successfully")
+@pytest.mark.PositiveOrganizations
+def test_get_organization_by_id(session_context):
+    response = ApiOrganizationsService.get_organization_by_id(session_context.org_id)
+    assert response.status_code == 200
+
+    data = GetOrganizationsById(**response.json())
+    assert data.id == session_context.org_id
+    assert data.name == session_context.organizations.org_name
