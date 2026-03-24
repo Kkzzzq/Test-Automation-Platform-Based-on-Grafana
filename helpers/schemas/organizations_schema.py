7410b0ca4809f183ccd91609f57aebf24ae75c8d
Kkzzzq <snorris068@gmail.com>
2026-03-24 14:00:20 +0800

    Update organizations_schema.py

--- helpers/schemas/organizations_schema.py
+++ helpers/schemas/organizations_schema.py
@@ -4,3 +4,17 @@ from pydantic import BaseModel
 class CreateOrganizationResponse(BaseModel):
     orgId: int
     message: str
+
+
+class AddUserInOrganizations(BaseModel):
+    message: str
+    userId: int
+
+
+class GetOrganizationsById(BaseModel):
+    id: int
+    name: str
+
+
+class UpdateUserInOrg(BaseModel):
+    message: str
